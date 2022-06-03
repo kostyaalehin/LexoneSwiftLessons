1b82e3ee8612df26fa4ae8c2d8215210e6c8dbb0
kostyaalehin <koss595959@gmail.com>
2022-06-03 17:38:25 +0500

    add files for future project

--- LexoneLesson03-MVP/LexoneLesson03-MVP/Models/Crypto.swift
+++ LexoneLesson03-MVP/LexoneLesson03-MVP/Models/Crypto.swift
@@ -6,11 +6,13 @@
 //
 
 import Foundation
+
 struct Crypto {
     var name: String
     var ticker: String
     var value: Int
 }
+
 extension Crypto {
     static var testData = [
         Crypto(name: "Bitcoin", ticker: "BTC", value: 55000),
