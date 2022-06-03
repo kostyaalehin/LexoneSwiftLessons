cc0b0f3655f2068b5954f587ff0001763bdd3de5
kostyaalehin <koss595959@gmail.com>
2022-06-03 17:37:57 +0500

    add array with crypto

--- LexoneLesson03-MVP/LexoneLesson03-MVP/Models/Crypto.swift
+++ LexoneLesson03-MVP/LexoneLesson03-MVP/Models/Crypto.swift
@@ -11,3 +11,12 @@ struct Crypto {
     var ticker: String
     var value: Int
 }
+extension Crypto {
+    static var testData = [
+        Crypto(name: "Bitcoin", ticker: "BTC", value: 55000),
+        Crypto(name: "Etherium", ticker: "ETH", value: 3500),
+        Crypto(name: "Ripple", ticker: "XRP", value: 589),
+        Crypto(name: "Stellar", ticker: "XLM", value: 20),
+        Crypto(name: "Algorand", ticker: "ALGO", value: 3)
+    ]
+}
