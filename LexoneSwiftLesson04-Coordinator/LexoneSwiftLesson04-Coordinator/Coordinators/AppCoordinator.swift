3800e76c468a39cc3ca69f43ad320ef139118467
kostyaalehin <koss595959@gmail.com>
2022-06-08 12:00:42 +0500

    add AppCoordinator

--- LexoneSwiftLesson04-Coordinator/LexoneSwiftLesson04-Coordinator/Coordinators/AppCoordinator.swift
+++ LexoneSwiftLesson04-Coordinator/LexoneSwiftLesson04-Coordinator/Coordinators/AppCoordinator.swift
@@ -5,4 +5,38 @@
 //  Created by Константин Алехин on 08.06.2022.
 //
 
-import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = FirstViewController.createObject()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+
+    func openFirstVC() {
+        let vc = FirstViewController.createObject()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func openSecondVC() {
+        let vc = SecondViewController.createObject()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func openThirdVC() {
+        let vc = ThirdViewController.createObject()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+
+}
