8cea8ec1d89d605b1518f5e2217fb6086ffd8afb
kostyaalehin <koss595959@gmail.com>
2022-06-09 23:29:03 +0500

    add struct Crypto

--- LexoneSwiftLesson05-MVС/LexoneSwiftLesson05-MVС/Models/Crypto.swift
+++ LexoneSwiftLesson05-MVС/LexoneSwiftLesson05-MVС/Models/Crypto.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Crypto {
+    var name: String
+    var ticker: String
+    var value: Int
+}
+
+extension Crypto {
+    static var testData = [
+        Crypto(name: "Bitcoin", ticker: "BTC", value: 55000),
+        Crypto(name: "Etherium", ticker: "ETH", value: 3500),
+        Crypto(name: "Ripple", ticker: "XRP", value: 589),
+        Crypto(name: "Stellar", ticker: "XLM", value: 20),
+        Crypto(name: "Algorand", ticker: "ALGO", value: 3)
+    ]
+}
