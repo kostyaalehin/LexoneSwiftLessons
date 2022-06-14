394a347e6c5c720a61a6f5bba8db877a7dfe17c5
kostyaalehin <koss595959@gmail.com>
2022-06-14 23:48:13 +0500

    create MVVM (model-view-viewModel
    https://www.youtube.com/watch?v=2yMtKZb-BO0

---
+++ LexoneSwiftLesson07-MVVM/LexoneSwiftLesson07-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LexoneSwiftLesson07-MVVM
+//
+//  Created by Константин Алехин on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
