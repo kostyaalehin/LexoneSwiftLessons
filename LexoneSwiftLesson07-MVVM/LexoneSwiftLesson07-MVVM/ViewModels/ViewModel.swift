373a74a6eb7b7b39a094bb7943ac966bce21cc7c
kostyaalehin <koss595959@gmail.com>
2022-06-16 14:51:56 +0500

    add statusColor

--- LexoneSwiftLesson07-MVVM/LexoneSwiftLesson07-MVVM/ViewModels/ViewModel.swift
+++ LexoneSwiftLesson07-MVVM/LexoneSwiftLesson07-MVVM/ViewModels/ViewModel.swift
@@ -6,7 +6,20 @@
 //
 
 import Foundation
+import UIKit.UIColor
 
 class ViewModel {
     var statusText = Dinamic("")
+    var statusColor = Dinamic(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+
+
+    func userButtonPressed(login: String, password: String) {
+        if login != User.logins[0].login || password != User.logins[0].password {
+            statusText.value = "Login failed!"
+            statusColor.value = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        } else {
+            statusText.value = "You saccessfully logged in."
+            statusColor.value = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        }
+    }
 }
