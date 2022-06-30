556de99ecb5b0f25fd7e6738931f2543b32b0682
kostyaalehin <koss595959@gmail.com>
2022-06-30 23:38:41 +0500

    add struct UserData

--- LexoneSwiftLesson07-MVVM/LexoneSwiftLesson07-MVVM/Models/UserData.swift
+++ LexoneSwiftLesson07-MVVM/LexoneSwiftLesson07-MVVM/Models/UserData.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct UserData {
+    var name: String?
+    var city: String?
+    var email: String?
+    var phone: String?
+}
+
+extension UserData {
+    static var userData = UserData(name: "Kostya", city: "Perm", email: "test@mail.com", phone: "+79998887766")
+}
