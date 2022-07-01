d69af8ce8ee011b0cf8ddd81d91099bdd740aa91
kostyaalehin <koss595959@gmail.com>
2022-07-01 22:43:41 +0500

    Optionals
    https://www.youtube.com/watch?v=H5HTwpmvSbo

---
+++ LexoneSwiftLesson08-Optionals.playground/Contents.swift
@@ -0,0 +1,21 @@
+let message: String = "This is message"
+let totalHieght: Double
+var points: Int
+var maxValue: Int
+
+print(message)
+
+
+// Character
+
+var fraction: String?
+var race: String
+var firstName: String?
+var lastName: String?
+var age: Int
+var adress: String?
+
+fraction = "Hobbit"
+print(fraction!)
+
+
