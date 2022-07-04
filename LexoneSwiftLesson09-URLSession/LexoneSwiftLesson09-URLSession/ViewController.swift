b5ee745afce68824ea67f32c0bb37a02abe4976e
kostyaalehin <koss595959@gmail.com>
2022-07-04 14:36:55 +0500

    URLSession
    https://www.youtube.com/watch?v=VzbG4GFAJNY

---
+++ LexoneSwiftLesson09-URLSession/LexoneSwiftLesson09-URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LexoneSwiftLesson09-URLSession
+//
+//  Created by Константин Алехин on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
